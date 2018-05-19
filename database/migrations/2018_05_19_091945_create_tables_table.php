67108f121021aef1c26c3e3b3fc63d2ee399ca1d
eyarijk <eyarij@gmail.com>
2018-05-19 12:56:02 +0300

    migrations

---
+++ database/migrations/2018_05_19_091945_create_tables_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tables', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('person_size');
+            $table->string('name')->nullable();
+            $table->text('description')->nullable();
+            $table->integer('reserve_price')->nullable();
+            $table->string('slug');
+            $table->boolean('is_reserved')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tables');
+    }
+}
--- database/migrations/2018_05_19_093442_create_orders_table.php
+++ database/migrations/2018_05_19_093442_create_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('table_id')->unsigned();
+            $table->integer('customer_id')->unsigned();
+            $table->text('note')->nullable();
+            $table->integer('total_price');
+            $table->string('payment');
+            $table->dateTime('time');
             $table->timestamps();
         });
     }
@@ -30,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders');
     }
 }
