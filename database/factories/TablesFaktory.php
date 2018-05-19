e763b7fafb9ed925619d3cb920a10214c7d47cb4
Vadym Panchenko <vadym.panchenko@ekreative.com>
2018-05-19 15:09:23 +0300

    fix tables faktory

--- database/factories/TablesFaktory.php
+++ database/factories/TablesFaktory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Table::class, function (Faker $faker) {
     return [
         'person_size' => $faker->numberBetween(2, 5),
-        'number' => $faker->numberBetween(1, 45),
+        'number' => $faker->unique()->numberBetween(1, 50),
         'description' => $faker->text,
         'reserve_price' => $faker->numberBetween(1000, 5000),
         'is_reserved' => $faker->numberBetween(0, 1),
